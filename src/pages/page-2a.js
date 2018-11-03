e736f710e4a1321eb3a0049c80a6401c00ef2072
scheehan <siowcheehan@gmail.com>
2018-11-04 04:22:45 +0800

    Add ignore file

---
+++ src/pages/page-2a.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Reasons I Should Consider JAMstack</h1>
+    <p>For faster development. A JAMstack website as it is just pre-built HTML and assets, all which can be served over a CDN.</p>
+    <Link to="/">What is JAMstack?</Link>
+  </Layout>
+)
+
+export default SecondPage
