bd35129bdf5d82700c63f1943fadf8abbb897cb8
Korhan Akcura <korhan.akcura@sgdigital.com>
2018-11-03 19:54:17 -0400

    Remove extra stuff.

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/raphaelgako/Documents/School_fcc/fcc_hackathon_jamstack/JAMsnack/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/raphaelgako/Documents/School_fcc/fcc_hackathon_jamstack/JAMsnack/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
