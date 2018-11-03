6c8a32eaf77c394ac1449788a19f103f5912e043
Korhan Akcura <korhan.akcura@sgdigital.com>
2018-11-03 16:29:05 -0400

    Remove node_modules.

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,11 +2,11 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/space/Hello/JAMstack/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/space/Hello/JAMstack/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/space/Hello/JAMstack/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-page-2-js": () => import("/space/Hello/JAMstack/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+  "component---cache-dev-404-page-js": () => import("/space/Hackletton15/JAMsnack/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/space/Hackletton15/JAMsnack/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/space/Hackletton15/JAMsnack/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/space/Hackletton15/JAMsnack/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
 }
 
-exports.data = () => import("/space/Hello/JAMstack/.cache/data.json")
+exports.data = () => import("/space/Hackletton15/JAMsnack/.cache/data.json")
 
